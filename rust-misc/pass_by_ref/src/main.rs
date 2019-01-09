5bbba20396854e67cd7f5539598c785eeb69f221
owenmyers <owendalemyers@gmail.com>
2019-01-09 07:39:10 -0500

    Added vector example to rust pass by reference.

--- rust-misc/pass_by_ref/src/main.rs
+++ rust-misc/pass_by_ref/src/main.rs
@@ -3,9 +3,19 @@ pub fn add_one(add_to: &mut i8) {
     *add_to += 1;
 }
 
+pub fn add_element_to_vec(vec: &mut Vec<u8>) {
+    vec.push(2);
+}
+
 fn main() {
     let mut x: i8 = 0;
     println!("Before add_one() x: {}", x);
     add_one(&mut x);
     println!("After add_one() x: {}", x);
+
+    let mut vec: Vec<u8> = Vec::new();
+    vec.push(1);
+    println!("Before add_element_to_vec() vec: {:?}", vec);
+    add_element_to_vec(&mut vec);
+    println!("After add_element_to_vec() vec: {:?}", vec);
 }
